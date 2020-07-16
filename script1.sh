312574fd3b184ac5cdfb16646a7a1df204827cce
rafaelklock <42119951+rafaelklock@users.noreply.github.com>
2020-07-16 16:26:20 -0300

    new
    new

---
+++ script1.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FOSRWARD ACCEPT
+
+
+echo "Regra Porta 5070 - Clientes Rede NET Nao Registram na 5060"
+iptables -t nat -A PREROUTING -i eno2 -p udp --dport 5070 -j REDIRECT --to-port 5060
+
+
+echo "Bloqueio de Friendly Scanner"
+iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "friendly-scanner" --algo bm --to 500 -j LOG --log-prefix '** SIP ATTACK **'
+iptables -A INPUT -p udp -m udp --dport 5060 -m string --string "friendly-scanner" --algo bm --to 500 -j DROP
+
+
+echo "Libera loopback"
+iptables -I INPUT 1 -i lo -j ACCEPT
+iptables -I INPUT -i lo -j ACCEPT
+iptables -I INPUT -s 127.0.0.1 -j ACCEPT
+
+
+echo "Liberando Hosts de Provedores VOIP"
+iptables -A INPUT -s 199.87.121.0/24 -j ACCEPT
+iptables -A INPUT -s 199.87.121.15/32 -j ACCEPT
+iptables -A INPUT -s 199.36.250.15/32 -j ACCEPT
+iptables -A INPUT -s 35.198.61.0/24 -j ACCEPT
+iptables -A INPUT -s 200.162.143.0/24 -j ACCEPT
+iptables -A INPUT -s 200.162.139.0/24 -j ACCEPT
+iptables -A INPUT -s 200.162.131.0/24 -j ACCEPT
+iptables -A INPUT -s 177.70.25.28/32 -j ACCEPT
+
+
+echo "Liberando Hosts confiaveis"
+iptables -A INPUT -s 148.72.153.72/32 -j ACCEPT
+iptables -A INPUT -s 189.85.161.98/32 -j ACCEPT
+iptables -A INPUT -s 189.34.44.107/32 -j ACCEPT
+
+echo "Bloqueio SMTP"
+iptables -A INPUT -p tcp --dport 25 -j LOG --log-prefix '** BLOQUEIO SMTP **'
+iptables -A INPUT -p tcp --dport 25 -j DROP
+
+echo "Bloqueio Mysql e Postgres"
+iptables -A INPUT -p tcp --dport 5432 -j LOG --log-prefix '** BLOQUEIO POSTGRESQL **'
+iptables -A INPUT -p tcp --dport 5432 -j DROP
+
+iptables -A INPUT -p tcp --dport 3306 -j LOG --log-prefix '** BLOQUEIO MYSQL **'
+iptables -A INPUT -p tcp --dport 3306 -j DROP
+
+
+
+
+echo "Reiniciando Fail2Ban"
+systemctl restart fail2ban
